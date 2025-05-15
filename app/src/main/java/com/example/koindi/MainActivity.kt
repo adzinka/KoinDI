d8c404f0cb81b503a417adf7397e656a4b40f6b1
Yelizaveta Zhurava <adzinkaliza@gmail.com>
2025-05-15 15:59:02 +0200

    temp: current progress snapshot

--- app/src/main/java/com/example/koindi/MainActivity.kt
+++ app/src/main/java/com/example/koindi/MainActivity.kt
@@ -4,26 +4,63 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.koindi.ui.theme.KoinDITheme
+import com.example.koindi.view.AuthViewModel
+import org.koin.android.scope.AndroidScopeComponent
+import org.koin.androidx.viewmodel.ext.android.getViewModel
+import org.koin.core.scope.Scope
 
-class MainActivity : ComponentActivity() {
+class MainActivity(override val scope: Scope) : ComponentActivity(), AndroidScopeComponent {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             KoinDITheme {
+
+                val viewModel = getViewModel<AuthViewModel>()
+                val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+                    if (uiState.isLoading) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            CircularProgressIndicator()
+                        }
+                    }
+
+                    if (uiState.error.isNotBlank()) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(
+                                text = uiState.error
+                            )
+                        }
+                    }
+                    uiState.name?.let {
+                        Greeting(
+                            name = it,
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                    }
                 }
             }
         }
--- app/src/main/java/com/example/koindi/view/AuthViewModel.kt
+++ app/src/main/java/com/example/koindi/view/AuthViewModel.kt
@@ -1,14 +1,50 @@
 package com.example.koindi.view
 
+import android.R.attr.name
+import android.R.id.message
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.koindi.domain.usecases.GetAddressUseCase
 import com.example.koindi.domain.usecases.GetUserNameUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.update
 
 class AuthViewModel(
     private val getUserNameUseCase: GetUserNameUseCase,
     private val getAddressUseCase: GetAddressUseCase
 ) : ViewModel() {
 
+    private val _uiState = MutableStateFlow(UiState())
+    val uiState = _uiState.asStateFlow()
+
+    init {
+        getUserName()
+    }
+
+    fun getUserName() = getUserNameUseCase()
+        .onStart {
+            _uiState.update { UiState(isLoading = true) }
+        }.onEach { result ->
+            _uiState.update { it.copy(name = result) }
+        }.catch { message ->
+            _uiState.update { it.copy(error = message.toString()) }
+        }.onCompletion {
+            _uiState.update { it.copy(isLoading = false) }
+        }
+        .launchIn(viewModelScope)
+
+    fun getAddress() = getAddressUseCase()
+        .onEach { result ->
+            _uiState.update { it.copy(address = result) }
+        }.catch { message ->
+            _uiState.update { it.copy(error = message.toString()) }
+        }.launchIn(viewModelScope)
 }
 
 data class UiState(
